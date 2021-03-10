167953aa5e56181da2974ab60f6e5ca6e352741d
Karim SAADI <karimsaadi.dev@gmail.com>
2021-03-10 11:00:02 +0000

    security - csurf

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ParkingModule } from './parking/parking.module';
 import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(ParkingModule, { cors: true });
   app.use(helmet());
+  app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }));
+  app.use(csurf());
   await app.listen(3000);
 }
 bootstrap();
