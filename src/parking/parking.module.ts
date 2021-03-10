feabbc2e39fc6e4f3bc670d01cea2ae6d21b7e39
Karim SAADI <karimsaadi.dev@gmail.com>
2021-03-10 11:00:37 +0000

    feat - bootstrap parking module elements

--- src/parking/parking.module.ts
+++ src/parking/parking.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { ParkingController } from './parking.controller';
+import { ParkingService } from './parking.service';
 
 @Module({
     imports: [
@@ -7,6 +9,8 @@ import { ThrottlerModule } from '@nestjs/throttler';
             ttl: 60,
             limit: 10,
         })
-    ]
+    ],
+    controllers: [ParkingController],
+    providers: [ParkingService],
 })
 export class ParkingModule { }
