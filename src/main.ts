412c8939d5cc04d08b2e714800341735fa07cbff
Karim SAADI <karimsaadi.dev@gmail.com>
2021-03-10 10:49:14 +0000

    security - helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ParkingModule } from './parking/parking.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(ParkingModule);
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
